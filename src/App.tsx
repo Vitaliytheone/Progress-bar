12bbe495ea06fd9789c9f9a3ad05f88dd0512c40
VITALIJ <Vitaliythesecond@gmail.com>
2022-03-11 23:58:21 +0300

    labels

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App() {
     return (
         <>
             <Title>Progress bars</Title>
-            <Progress variant="line" />
+            <Progress variant="line" label="Line bar" />
+            <Progress variant="line" label="Circle bar" />
         </>
     );
 }
