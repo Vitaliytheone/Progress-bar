a12befca4aa7d4df5a898ade8f3477e02bbbfc7e
VITALIJ <Vitaliythesecond@gmail.com>
2022-03-27 23:33:20 +0300

    removed comment

--- src/components/progress/index.tsx
+++ src/components/progress/index.tsx
@@ -6,7 +6,6 @@ const Progress = ({ variant, label, withValue, maxValue, value }: TProgress) => 
     const isLine = variant === "line";
     const number = { v: Number(value), m: Number(maxValue) };
     const valueInPercent = `${number.m > 0 ? ((number.v * 100) / number.m).toFixed(0) : 0}%`;
-    // const width = number.v <= number.m ? valueInPercent : number.m;
 
     return (
         <div>
