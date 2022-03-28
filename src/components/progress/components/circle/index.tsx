3ad72b9d06aa454182712b32a16962efab75b01d
VITALIJ <Vitaliythesecond@gmail.com>
2022-03-28 20:37:01 +0300

    svg

--- src/components/progress/components/circle/index.tsx
+++ src/components/progress/components/circle/index.tsx
@@ -1,7 +1,21 @@
 import styles from "../../styles.module.css";
 
 const CircleBar = ({ valueInPercent, withValue }: any) => {
-    return <div className={withValue ? styles.progressInfoValueLine : styles.progressInfo}>Circle Bar</div>;
+    return (
+        <div className={styles.circleProgress}>
+            <svg className={styles.circleSvg} height="120px" width="120px">
+                <circle
+                    className="progress-ring__circle"
+                    stroke-width="10"
+                    stroke="white"
+                    fill="transparent"
+                    r="30"
+                    cx="50"
+                    cy="50"
+                />
+            </svg>
+        </div>
+    );
 };
 
 export default CircleBar;
