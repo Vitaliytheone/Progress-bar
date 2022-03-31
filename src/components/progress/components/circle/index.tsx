b53de940701986ff998282cacee2fd489567aa61
VITALIJ <Vitaliythesecond@gmail.com>
2022-03-31 23:47:26 +0300

    usememo

--- src/components/progress/components/circle/index.tsx
+++ src/components/progress/components/circle/index.tsx
@@ -1,15 +1,16 @@
+import { useMemo } from "react";
 import styles from "../../styles.module.css";
 import { TCircle } from "../../type";
 import { getCircleParams } from "./helpers";
 
 const CircleBar = ({ valueInPercent, withValue, stroke = 10, width = 110, height = 110 }: TCircle) => {
-    const { r, h, w, cy, cx } = getCircleParams({ stroke, width, height });
+    const { r, h, w, cy, cx } = useMemo(() => getCircleParams({ stroke, width, height }), [stroke, width, height]);
     return (
         <div className={styles.circleProgress} style={{ width: w, height: h }}>
             <svg className={styles.circleSvg} height={h} width={w}>
                 <circle
                     className="progress-ring__circle"
-                    stroke-width={stroke}
+                    strokeWidth={stroke}
                     stroke="red"
                     fill="transparent"
                     r={r}
